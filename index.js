07c6a08c278f9e1aa1307bdf00bf8b2d0b965e0a
Justin <jdlow2@gmail.com>
2019-01-21 18:51:44 -0500

    added auth route, hashed pw

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const bcrypt = require('bcryptjs');
 const db = require('./database/dbConfig.js');
 
 const server = express();
@@ -15,11 +15,30 @@ server.get('/', (req, res) => {
 // protect this route, only authenticated users should see it
 server.get('/api/users', (req, res) => {
   db('users')
-    .select('id', 'username')
+    .select('id', 'username', 'password') // added password to response ***DON'T DO THIS! EVER!!***
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
+
+server.post('/api/register', (req, res) => {
+  // grab username and password from body
+  const creds = req.body;
+
+  // generate the hash from the pw
+  const hash = bcrypt.hashSync(creds.password, 14); // rounds is 2^x ie 2^14
+
+  // override pw with hash
+  creds.password = hash;
+
+  // save user to db
+  db('users')
+    .insert(creds)
+    .then(ids => {
+      res.status(200).json(ids)
+    })
+    .catch(err => res.status(400).json(err));
+})
 
 server.listen(3300, () => console.log('\nrunning on port 3300\n'));
