1df17958ae81928c008e3aabb99e466dbd8c0a2b
Justin <jdlow2@gmail.com>
2019-01-21 19:09:44 -0500

    login function using comparesync

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ server.post('/api/register', (req, res) => {
 
   // generate the hash from the pw
   const hash = bcrypt.hashSync(creds.password, 14); // rounds is 2^x ie 2^14
+  console.log(bcrypt.getRounds(hash));
 
   // override pw with hash
   creds.password = hash;
@@ -36,9 +37,29 @@ server.post('/api/register', (req, res) => {
   db('users')
     .insert(creds)
     .then(ids => {
-      res.status(200).json(ids)
+      res.status(200).json(ids);
     })
     .catch(err => res.status(400).json(err));
-})
+});
+
+server.post('/api/login', (req, res) => {
+  // grab credentials
+  const creds = req.body;
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      // user compareSync to compare pw to hashed password
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        // passwords match and user exists
+        res.status(200).json({message: 'welcome!'});
+      } else {
+        // either username is invalid or password is wrong
+        res.status(401).json({ error: 'Authentication failed' });
+      }
+    })
+    .catch(err => res.status(400).json(err));
+});
 
 server.listen(3300, () => console.log('\nrunning on port 3300\n'));
